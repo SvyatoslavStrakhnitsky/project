9e89d3c879a7be1f93e4dacda6c551dd57d9da86
SvyatoslavStrakhnitsky <sviatoyyy@gmail.com>
2023-03-08 19:30:41 +0200

    sidebar

--- src/app/layout/ui/AppLayout.tsx
+++ src/app/layout/ui/AppLayout.tsx
@@ -1,11 +1,15 @@
 import { Header } from "@/widgets/Header";
 import { MainContent } from "@/widgets/MainContent";
+import { Sidebar } from "@/widgets/Sidebar";
 
 export const AppLayout = () => {
   return (
     <>
       <Header />
-      <MainContent />
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <MainContent />
+      </div>
     </>
   );
 };
---
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -0,0 +1,35 @@
+import { FC, useState } from "react";
+import { classNames } from "@/shared/lib/classNames/classNames";
+import { Button } from "@/shared/ui/Button";
+import DoubleArrowLeft from "@/shared/assets/icons/double-arrow-left.svg";
+import DoubleArrowRight from "@/shared/assets/icons/double-arrow-right.svg";
+import cls from "./Sidebar.module.css";
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar: FC<SidebarProps> = (props) => {
+  const { className } = props;
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onSidebarToggle = () => {
+    setCollapsed((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <Button
+        className={cls.btnCollapse}
+        onClick={onSidebarToggle}
+        theme={"clear"}
+      >
+        {collapsed ? <DoubleArrowRight /> : <DoubleArrowLeft />}
+      </Button>
+    </div>
+  );
+};
