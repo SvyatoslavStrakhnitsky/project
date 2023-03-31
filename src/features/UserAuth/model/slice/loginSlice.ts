2f7049b2bd2944808f110486481f0e2ab6fdc934
SvyatoslavStrakhnitsky <sviatoyyy@gmail.com>
2023-03-31 14:57:03 +0300

    deploy fix

---
+++ src/features/UserAuth/model/slice/loginSlice.ts
@@ -0,0 +1,24 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/LoginSchema';
+
+const initialState: LoginSchema = {
+    username: '',
+    password: '',
+    isOpen: false
+};
+
+export const { 
+    actions: loginActions,
+    reducer: loginReducer
+} = createSlice({
+    name: 'login',
+    initialState,
+    reducers: {
+        setUsername: (state, action: PayloadAction<string>) => {
+            state.username = action.payload;
+        },
+        setPassword: (state, action: PayloadAction<string>) => {
+            state.password = action.payload;
+        },
+    }
+});
